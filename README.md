96a6e0e33e67fb6db1ee72f9b4ecfe2bacb9a61a
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-01-13 00:53:08 +0100

    add initial components

---
+++ README.md
@@ -0,0 +1,6 @@
+Dependencies:
+
+- React Native Styler
+- React Native Component Styler
+- moment.js
+- React Native SVG (needs to be linked)
--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 import Avatar from './lib/avatar';
+import BackgroundGradient from './lib/background-gradient';
+import BackgroundImage from './lib/background-image';
+import BottomAction from './lib/bottom-action';
+import Box from './lib/box';
+import BoxParagraph from './lib/box-paragraph';
+import BoxTitle from './lib/box-title';
+import Button from './lib/button';
+import ButtonGroup from './lib/button-group';
+import Center from './lib/center';
+import DateIndicator from './lib/date-indicator';
+import Emoji from './lib/emoji';
+import Form from './lib/form';
+import Input from './lib/input';
+import Label from './lib/label';
+import ListItem from './lib/list-item';
+import Padding from './lib/padding';
+import QuickMessage from './lib/quick-message';
+import Stepper from './lib/stepper';
+import TabBar from './lib/tab-bar';
 
 export {
-    Avatar
+    Avatar,
+    BackgroundGradient,
+    BackgroundImage,
+    BottomAction,
+    Box,
+    BoxParagraph,
+    BoxTitle,
+    Button,
+    ButtonGroup,
+    Center,
+    DateIndicator,
+    Emoji,
+    Form,
+    Input,
+    Label,
+    ListItem,
+    Padding,
+    QuickMessage,
+    Stepper,
+    TabBar
 };
