ff189665e2442f72a9d6464031ebe7b85ac4f815
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-01-13 15:18:06 +0100

    ignore .docz

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "index" */ 'index.mdx'),
-  'lib/avatar/Avatar.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "lib-avatar-avatar" */ 'lib/avatar/Avatar.mdx'),
-}
