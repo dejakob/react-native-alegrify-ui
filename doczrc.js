2384abc1350f9d6f504a2684da4c1044f86e2a93
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-03-16 21:49:05 +0100

    add docs

---
+++ doczrc.js
@@ -0,0 +1,15 @@
+export default {
+    native: true,
+    title: 'RN Alegrify UI',
+    htmlContext: {
+        head: {
+            raw: `<style>
+                .react-live-preview { background-color: #ddd; }
+            </style>
+            `
+        }
+    },
+    themeConfig: {
+        showPlaygroundEditor: true
+    }
+};
